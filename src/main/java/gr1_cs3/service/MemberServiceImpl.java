f5bfa4c83ef5d9b66b2d795491d3968235f91ca3
tekdoken <tekdokenzx@gmail.com>
2021-12-05 00:28:03 +0700

    interface

--- src/main/java/gr1_cs3/service/MemberServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package gr1_cs3.service;
-
-public class MemberServiceImpl implements MemberService<Member>{
-}
---
+++ src/main/java/gr1_cs3/service/implement/ProductServiceImpl.java
@@ -0,0 +1,6 @@
+package gr1_cs3.service.implement;
+
+import gr1_cs3.service.GeneralService;
+
+public class ProductServiceImpl implements GeneralService<Product> {
+}
