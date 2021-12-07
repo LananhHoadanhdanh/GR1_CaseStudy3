43e426da388f2b5f51e0030e7023496709b2f788
DuyLeVu <levuduy132@gmail.com>
2021-12-07 12:04:57 +0700

    create category servlet

--- src/main/java/gr1_cs3/service/CategoryService.java
+++ src/main/java/gr1_cs3/service/CategoryService.java
@@ -1,6 +1,10 @@
 package gr1_cs3.service;
 
 import gr1_cs3.model.Category;
+import gr1_cs3.model.Product;
+
+import java.util.List;
 
 public interface CategoryService extends GeneralService<Category> {
+    List<Product> getProductByCID(int cid);
 }
