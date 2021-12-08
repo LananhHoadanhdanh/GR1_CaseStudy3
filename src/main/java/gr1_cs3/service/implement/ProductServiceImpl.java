1325f5f4be6ad1accc0e4526c6b42b3d91d28f50
DuyLeVu <levuduy132@gmail.com>
2021-12-08 14:36:41 +0700

    commit before pull dev

--- src/main/java/gr1_cs3/service/implement/ProductServiceImpl.java
+++ src/main/java/gr1_cs3/service/implement/ProductServiceImpl.java
@@ -49,7 +49,8 @@ public class ProductServiceImpl implements ProductService {
     public List<Product> printFourProduct() throws SQLException {
         List<Product> products = new ArrayList<>();
         try (Connection connection = getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement("select * from product where product.quantity > 0 order by id desc limit 4");) {
+             PreparedStatement preparedStatement = connection.prepareStatement("select * from product where product.quantity > " +
+                     " 0 order by id desc limit 4");) {
             ResultSet rs = preparedStatement.executeQuery();
             while (rs.next()) {
                 int id = rs.getInt("id");
